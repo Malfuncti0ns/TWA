a0b694b931eb4dfee2df023430c25468381640a0
Malfuncti0ns <matthewchiandotto1@gmail.com>
2024-05-21 14:12:14 +1000

    Comments for site

--- Assignment/TWAprojectSpec2024/Login.php
+++ Assignment/TWAprojectSpec2024/Login.php
@@ -6,10 +6,12 @@ require_once ('dbconn.php');
 $user = $_POST["username"];
 $pass = $_POST["password"];
 
-$sql = "SELECT 'password', is_admin FROM users WHERE email = '$user'";
+$sql = "SELECT password, is_admin FROM users WHERE email = '$user'";
 $results = $dbConn->query($sql);
 
 //Pulling the password and role from the table users where the email is the username (I've committed to calling it username now)
+
+//Previous had SELECT 'password' which was causing issues with which password it was pulling from (Form or DB), removed the ' ' and got the result I was looking for
 
 if ($results->num_rows > 0) {
     $row = $results->fetch_assoc();
@@ -18,16 +20,21 @@ if ($results->num_rows > 0) {
 
     //checking input for matches and pulling the password and is_admin rows for each true result if there is a result > 0 otherwise it will break with DOM message
 
-    if (password_verify($pass, $password_hash)) {
+    //Add password from session to a hash within the php and compare below
+
+    $input_hash = hash('sha256', $pass);
+
+    if ($input_hash === $password_hash ) {
         $_SESSION['username'] = $user;
         $_SESSION['role'] = $role;
 
-        if ($role === 1) {
+        if ($role === '1') {
+            //Here I made the mistake of leaving just 1 because it was looking for an int instead of the literal 1, added ' ' and got an admin to login to the
+            //right page
             header('Location: admin.php');
         } else {
             header('Location: log.php');
         }
-        exit();
     }  else {
     echo "<p>Invalid username or password</p>";
 //it's hitting the PHP page on sucess but going all the way to this break
