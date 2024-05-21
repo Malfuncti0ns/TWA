7091966268f105b55ca037f170943e142a785418
Malfuncti0ns <matthewchiandotto1@gmail.com>
2024-05-21 14:23:58 +1000

    It's working! ITS WORKING!
    The DB is updated with the last login because I am an SQL
    newbie who finally got it working

---
+++ Assignment/TWAprojectSpec2024/Last_Login.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+require_once('dbconn.php');
+//testing this script, to see if the DB updates logout times. 
+//Will run when a logout button is on the Admin or Workout page
+
+if (isset($_SESSION['username'])) {
+    
+    $username = $_SESSION['username'];
+
+    $updateLogin = "UPDATE users SET last_login = NOW() WHERE email = '$username'";
+    $dbConn->query($updateLogin);
+    session_destroy();
+    header('Location: login_form.html');
+    exit();
+}
+
+//ITS WORKING...ITS WORKING!
