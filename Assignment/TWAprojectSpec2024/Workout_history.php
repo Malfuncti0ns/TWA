28d42ba21ab3917451e7346ceb47392f5ca63096
Malfuncti0ns <matthewchiandotto1@gmail.com>
2024-05-27 14:39:10 +1000

    Fixed my mistake
    Need to think before I code

--- Assignment/TWAprojectSpec2024/Workout_history.php
+++ Assignment/TWAprojectSpec2024/Workout_history.php
@@ -1,36 +1,18 @@
 <?php
 session_start();
-require_once("dbconn.php");
+require_once ("dbconn.php");
+$user = $_SESSION['user_id'];
 
-$sql = "SELECT * FROM workout WHERE user_id = ? ORDER BY workout_date DESC";
-$result = $dbConn->prepare($sql);
-$stmt->bind_param("i", $_SESSION["user_id"]);
-$stmt->execute();
-$result = $stmt->get_result();
-
-//Similar to what I did in admin_reg, getting the result from a query but instead of storing in ->store_result() I'm adding it to a variable 
-//for use later and we can iterate over again when you filter
-
-$startdate = $_POST["date_start"];
-$enddate= $_POST["date_end"];
-$exercise = $_POST["exercise"];
-
-$sql = "SELECT * FROM workout WHERE user_id = ?";
-if(!empty($startdate) && !empty($enddate)) {
-    $sql = "SELECT * FROM workout WHERE user_id = ? AND workout_date BETWEEN ? AND ? ORDER BY workout_date DESC";
-    //https://www.w3schools.com/sql/sql_between.asp using between the dates if either entry is empty (Can't be empty but just in case)
-}
-if(!empty($exercise)){
-    $sql = "SELECT * FROM worout WHERE user_id = ? AND exercise_id = ? ORDER BY workout_date DESC";
-}
-
-//stuck here wil come back
-
+$sql = "SELECT * FROM workout WHERE user_id = $user ORDER BY workout_date";
+$result = $dbConn->query($sql);
+#My brain started working, I'm not inputting data into user_id because I'm pulling it from the session
+#I can sort by $user
 ?>
-
 <!--Because this will be all in one page, I can set this up like I would log_it_in_eddie and put my PHP script within the page
 thinking being that it will still pull the session data and then hit the DB for further queries-->
 
+<!--Nuked the whole thing I had before, why am I making this difficult for myself, I have an example from an exercise we did
+just create the table and fill it with the data from the database and then let people filter-->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -41,33 +23,38 @@ thinking being that it will still pull the session data and then hit the DB for 
     <link rel="stylesheet" href="../project/css/project_master.css">
     <script type="text/javascript" src="../project/javascript/project_Script.js"></script>
 </head>
-
 <!--Thoughts before I take a break from coding the assignment, this will require a search to be done on itself so like an 
 exercise we did, I can use $_SERVER["PHP_SELF"] to refer back on itself but after I query the database so that the page
 has the data from the databse and once a filter is applied, its applying it to the stored data on the page-->
 
-
 <body>
+
     <a href="Index.php"><img class="banner" src="../project/images/logo.png" alt="Function Tracker Logo"></a>
     <div class="Navigation">
         <a href="Workout_log.php">Workout log</a>
         <a href="Workout_stats.php">Workout Statistics</a>
     </div>
-    <method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-    <label for="date_start">Start date:</label>
-    <input type="date" id="date_start" name="date_start">
-    <label for="date_end">End date:</label>
-    <input type="date" id="end_date" name="end date">
-    <label for="exercise">Exercise Type:</label>
-    <select id="exercise" name="exercise">
-    <option value="">All</option>
-    <option value="1">Walking</option>
-    <option value="2">Running</option>
-    <option value="3">Cycling</option>
-    </select>
-    <input type="submit">
-    </form>
-
+    <table>
+        <tr>
+        <th>Workout Date</th>
+        <th>Exercise</th>
+        <th>Duration</th>
+        <th>Distance</th>
+        <th>Notes</th>
+        </tr>
+        <?php
+        while ($row = $result->fetch_assoc()) { ?>
+            <tr>
+                <td><?php echo $row["workout_date"]; ?></td>
+                <td><?php echo $row["exercise_id"]; ?></td>
+                <td><?php echo $row["duration"]; ?></td>
+                <td><?php echo $row["distance"]; ?></td>
+                <td><?php echo $row["notes"]; ?></td>
+            </tr>
+        <?php }
+        $dbConn->close(); ?>
+    </table>
     <a href="Last_Login.php">Logout!</a>
+</body>
 
 </html>
