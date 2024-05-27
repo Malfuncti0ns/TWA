f98917bd4a8bbe356e34e82471cfacac8c6d891b
Malfuncti0ns <matthewchiandotto1@gmail.com>
2024-05-27 13:28:04 +1000

    Hitting a wall
    Taking a break until I can think things through

--- Assignment/TWAprojectSpec2024/Workout_history.php
+++ Assignment/TWAprojectSpec2024/Workout_history.php
@@ -6,11 +6,25 @@ $sql = "SELECT * FROM workout WHERE user_id = ? ORDER BY workout_date DESC";
 $result = $dbConn->prepare($sql);
 $stmt->bind_param("i", $_SESSION["user_id"]);
 $stmt->execute();
-$result = $stmt->get_result()
+$result = $stmt->get_result();
 
 //Similar to what I did in admin_reg, getting the result from a query but instead of storing in ->store_result() I'm adding it to a variable 
 //for use later and we can iterate over again when you filter
 
+$startdate = $_POST["date_start"];
+$enddate= $_POST["date_end"];
+$exercise = $_POST["exercise"];
+
+$sql = "SELECT * FROM workout WHERE user_id = ?";
+if(!empty($startdate) && !empty($enddate)) {
+    $sql = "SELECT * FROM workout WHERE user_id = ? AND workout_date BETWEEN ? AND ? ORDER BY workout_date DESC";
+    //https://www.w3schools.com/sql/sql_between.asp using between the dates if either entry is empty (Can't be empty but just in case)
+}
+if(!empty($exercise)){
+    $sql = "SELECT * FROM worout WHERE user_id = ? AND exercise_id = ? ORDER BY workout_date DESC";
+}
+
+//stuck here wil come back
 
 ?>
 
@@ -37,9 +51,22 @@ has the data from the databse and once a filter is applied, its applying it to t
     <a href="Index.php"><img class="banner" src="../project/images/logo.png" alt="Function Tracker Logo"></a>
     <div class="Navigation">
         <a href="Workout_log.php">Workout log</a>
-        <<a href="Workout_stats.php">Workout Statistics</a>
+        <a href="Workout_stats.php">Workout Statistics</a>
     </div>
-
+    <method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <label for="date_start">Start date:</label>
+    <input type="date" id="date_start" name="date_start">
+    <label for="date_end">End date:</label>
+    <input type="date" id="end_date" name="end date">
+    <label for="exercise">Exercise Type:</label>
+    <select id="exercise" name="exercise">
+    <option value="">All</option>
+    <option value="1">Walking</option>
+    <option value="2">Running</option>
+    <option value="3">Cycling</option>
+    </select>
+    <input type="submit">
+    </form>
 
     <a href="Last_Login.php">Logout!</a>
 
